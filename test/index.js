2164761d4313d7fd8e50c268d7b744f27e3d8b5e
Tom <thomas.chambrier@astrosat.biz>
2016-09-16 12:37:47 +0100

    Added test boilerplate

--- test/index.js
+++ test/index.js
@@ -4,3 +4,44 @@ var chai = require('chai');
 var expect = require('chai').expect;
 
 var index = require('..\\src\\index.js');
+
+
+describe( 'Top level suite', function() {
+ 
+    describe( 'Second level test suite', function() {
+ 
+        before( function() {
+            // executes once, before all tests 
+        } );
+ 
+        beforeEach( function() {
+            // executes before each test of the suite
+        } );
+ 
+        after( function() {
+            // executes once, after all tests
+        } );
+ 
+        afterEach( function() {
+            // executes after each test of the suite
+        } );
+ 
+        it( 'should verify a behavior', function() {
+            // a test containing assertions
+        } );
+ 
+        xit( 'should be a pending test', function() {
+            // this test is pending. All assertions inside are ignored.
+        } );    
+ 
+        it.skip( 'should also be a pending test', function() {
+            // it.skip is a longer, more semantic form of xit
+        } );        
+ 
+        it( 'should also be a pending test' );
+    } );
+ 
+    describe( 'Another second level test suite', function() {
+ 
+    } );
+} );
