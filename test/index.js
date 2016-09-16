cbb7f4777aea7d9351750ac4fea2bd7e45a9f375
Tom <tomw08@gmail.com>
2016-09-16 17:17:45 +0100

    added first test

--- test/index.js
+++ test/index.js
@@ -8,89 +8,56 @@ var index = require('../src/index.js');
 
 var listing1 = fs.readFileSync('./test/fixtures/listing1.txt', "utf8");
 var listing2 = fs.readFileSync('./test/fixtures/listing2.txt', "utf8");
-const TESTING = true;
 
+describe('handles input', function() {
+ 
+    describe('when given', function() {
+                         let t = 7
 
-describe( 'handles input', function() {
- 
-    describe( 'when given', function() {
- 
         before( function() {
             // executes once, before all tests 
 
-            var t = 7
-        } );
+        });
  
         beforeEach( function() {
             // executes before each test of the suite
-        } );
+
+        });
  
         after( function() {
             // executes once, after all tests
-        } );
+        });
  
         afterEach( function() {
             // executes after each test of the suite
-        } );
- 
-        it( 'should verify a behavior', function() {
+        });
+        
+        it('an arg not of string type', function() {
             // a test containing assertions
-            t.should.equal(1);
-        } );
- 
-        xit( 'should be a pending test', function() {
-            // this test is pending. All assertions inside are ignored.
-        } );    
- 
-        it.skip( 'should also be a pending test', function() {
-            // it.skip is a longer, more semantic form of xit
-        } );        
- 
-        it( 'should also be a pending test' );
-    } );
- 
-    describe( 'Another second level test suite', function() {
- 
-    } );
-} );
+            expect(index.extracts).to.be.defined;
+    
+        });
 
-
-describe( 'extracts data', function() {
- 
-    describe( 'Second level test suite', function() {
- 
-        before( function() {
-            // executes once, before all tests 
-        } );
- 
-        beforeEach( function() {
-            // executes before each test of the suite
-        } );
- 
-        after( function() {
-            // executes once, after all tests
-        } );
- 
-        afterEach( function() {
-            // executes after each test of the suite
-        } );
- 
-        it( 'should verify a behavior', function() {
+        it('an arg not of string type', function() {
             // a test containing assertions
-        } );
+            expect(
+                function getData () {
+                    index.extracts(6);
+                }).to.throw(TypeError);
+
+            expect(
+                function getData () {
+                    index.extracts({'t':1});
+                }).to.throw(TypeError);
+
+            expect(
+                function getData() {
+                    index.extracts();
+                }).to.throw(TypeError);
+        });
+    });
  
-        xit( 'should be a pending test', function() {
-            // this test is pending. All assertions inside are ignored.
-        } );    
+    describe('Another second level test suite', function() {
  
-        it.skip( 'should also be a pending test', function() {
-            // it.skip is a longer, more semantic form of xit
-        } );        
- 
-        it( 'should also be a pending test' );
-    } );
- 
-    describe( 'Another second level test suite', function() {
- 
-    } );
-} );
+    });
+});
