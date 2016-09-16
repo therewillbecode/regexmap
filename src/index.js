c5aa828543a5e3984277a376906b289f49040672
Tom <tomw08@gmail.com>
2016-09-16 18:34:20 +0100

    Added file for listing regex class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 'use strict';
 
-  let privateVariable = 10;
+var listingRegexes = require('./src/index.js');
 
-  let privateMethod = () => {
+let privateVariable = 10;
+
+let privateMethod = () => {
     console.log('Inside a private method!');
     privateVariable++;
-  }
+}
 
-  let getData = () => {
-    console.log('This is a method I want to expose!');
-  }
+let getData = () => {
+  console.log('This is a method I want to expose!');
+}
 
 
 module.exports = {
---
+++ src/listingRegexes.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class listingRegexes {
+	this.nightlyRate:
+	this.currency:
+	this.propertyType: / /;
+    this.noBedrooms
+	this.noBeds:
+	this.noGuests:
+	this.cleaningFee
+	this.securityDeposit:
+	this.cancellationType:
+	this.description:
+	this.houseRules:
+	this.safetyFeatures:
+	this.availability:
+	this.noReviews:
+	this.reviewRating:
+	this.hostName:
+	this.hostProfileUrl:
+	this.hostResponseRate:
+	this.hostResponseTime:
+	this.hostVerified:
+	this.hostDescription:
+	this.instantBook:
+	this.hostProfileCreationDate:
+	this.isBusinessReady:
+}
+
+module.exports = {
+  listingRegexes: listingRegexes
+} 
