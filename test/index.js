0229ccdfa37db9f110978c830b751fbf09ef6737
Tom <tomw08@gmail.com>
2016-09-18 10:05:57 +0100

    code style

--- test/index.js
+++ test/index.js
@@ -9,55 +9,58 @@ var index = require('../src/index.js');
 var listing1 = fs.readFileSync('./test/fixtures/listing1.txt', "utf8");
 var listing2 = fs.readFileSync('./test/fixtures/listing2.txt', "utf8");
 
-describe('handles input', function() {
+
  
-    describe('when given', function() {
+describe('when given', function() {
                          let t = 7
 
-        before( function() {
-            // executes once, before all tests 
+    before( function() {
+        // executes once, before all tests 
 
-        });
+    });
  
-        beforeEach( function() {
-            // executes before each test of the suite
+    beforeEach( function() {
+        // executes before each test of the suite
 
-        });
+    });
  
-        after( function() {
-            // executes once, after all tests
-        });
+    after( function() {
+        // executes once, after all tests
+    });
  
-        afterEach( function() {
-            // executes after each test of the suite
-        });
+    afterEach( function() {
+        // executes after each test of the suite
+    });
         
-        it('an arg not of string type', function() {
-            // a test containing assertions
-            expect(index.extracts).to.be.defined;
-    
-        });
-
-        it('an arg not of string type', function() {
-            // a test containing assertions
-            expect(
-                function getData () {
-                    index.extracts(6);
-                }).to.throw(TypeError);
-
-            expect(
-                function getData () {
-                    index.extracts({'t':1});
-                }).to.throw(TypeError);
-
-            expect(
-                function getData() {
-                    index.extracts();
-                }).to.throw(TypeError);
-        });
+    it('an arg not of string type', function() {
+        // a test containing assertions
+        expect(index.extracts).to.be.defined;
+  
     });
- 
-    describe('Another second level test suite', function() {
- 
+
+    it('an arg not of string type', function() {
+        // a test containing assertions
+        expect(
+            function getData () {
+                index.extracts(6);
+            }).to.throw(TypeError);
+
+        expect(
+            function getData () {
+                index.extracts({'t':1});
+            }).to.throw(TypeError);
+
+        expect(
+            function getData() {
+                index.extracts();
+            }).to.throw(TypeError);
     });
+});
+ 
+describe('Regex for', function() {
+    it('location should give', function() {
+        // a test containing assertions
+        expect(listing1.match(listingRegexes.location)).to.equal.('London');
+    });
+
 });
