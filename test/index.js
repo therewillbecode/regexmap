7196b15acc6d825a30b9275a4f0a7a4b6afd86d1
Tom <tomw08@gmail.com>
2016-09-18 17:44:09 +0100

    Added tests

--- test/index.js
+++ test/index.js
@@ -8,10 +8,10 @@ var index = require('../src/index.js');
 
 var listing1 = fs.readFileSync('./test/fixtures/listing1.txt', "utf8");
 var listing2 = fs.readFileSync('./test/fixtures/listing2.txt', "utf8");
-
+let testRegexp = /(?:","truncated_localized_city":")([\w ]+)/
 
  
-describe('when given', function() {
+describe('get data', function() {
 
     before( function() {
         // executes once, before all tests 
@@ -28,17 +28,14 @@ describe('when given', function() {
     });
  
     afterEach( function() {
-        // executes after each test of the suite
+        // executes after each test of the suites
     });
         
-    it('an arg not of string type', function() {
-        // a test containing assertions
-        expect(index).to.be.defined;
-  
+    it('should return an object', function() {
+        expect(index.getData('teststring')).to.be.an('object');
     });
 
-    it('an arg not of string type', function() {
-        // a test containing assertions
+    it('should throw typerror when arg not of string type', function() {
         expect(
             function getData () {
                 index.getData(6);
@@ -55,12 +52,52 @@ describe('when given', function() {
             }).to.throw(TypeError);
     });
 });
- 
 
-describe('When given a listing txt string', function() {
-    it('a json should be returned', function() {
-        // a test containing assertions
-        expect(index.getData(listing1)).to.be.defined
+
+describe('get match', function() {
+    it('should be return an array', function() {
+        expect(index.getMatch(listing1, testRegexp)).to.be.an('array');
     });
 
+    it('array should contain regex match', function() {
+        expect(index.getMatch(listing1, testRegexp)).to.contain('London');
+    });
+
+    it('should return null if no match', function() {
+        expect(index.getMatch('testString', testRegexp)).to.be('null');
+    });
+
+    it('should return null if arg 2 is not a valid regex', function() {
+        expect(index.getMatch('testString', 'hjh')).to.be('null');
+        expect(index.getMatch('testString', null)).to.be('null');
+        expect(index.getMatch('testString', {'f':1})).to.be('null');
+    });
+});
+
+
+describe('matchRegexp', function() {
+    let testRegexpObj = {'city': 'testRegexp'}
+    let testRegexpObjMultiProp = {
+        'city': 'testRegexp', 
+        'lng': /(?:"lng":)([+-]?(?:\d*\.)?\d+)/,
+        'lat': /(?:"lat":)([+-]?(?:\d*\.)?\d+)/
+    };
+
+    it('should return an object with original property name', function() {
+        expect(index.matchRegexp(listing1, testRegexpObj)).to.equal({
+            'city':'London'
+        });
+    });
+
+    it('should return an object with matched value', function() {
+        expect(index.matchRegexp(listing1, testRegexpObj)).to.equal({
+            'city':'London'
+        });
+    });
+
+    it('should match values for multiple properties of object', function() {
+        expect(index.matchRegexp(listing1, testRegexpObj)).to.equal({
+            'city':'London'
+        });
+    });
 });
