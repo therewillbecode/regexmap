97baf1da715023b8e4ddec77530ed482dd38560e
Tom <tomw08@gmail.com>
2016-09-20 19:48:32 +0100

    Removed getData function

--- src/index.js
+++ src/index.js
@@ -11,15 +11,6 @@ let fs = require('fs');
 let listing1 = fs.readFileSync('./test/fixtures/listing1.txt', "utf8");
 
 
-// RegexObj stores list of regular expressions to extract each data point
-let getData = (ListingText, regexObj) => {
-  if (ListingText === undefined){
-    return {}
-}
-
-    return mapRegexps(regexpObject)
-}
-
 // maps dict like object of regular expressions to their match results for a given string
 let mapRegexps = (regexpDict, sourceTxt) => _.mapValues(regexpDict, (val, key, obj) => {
    if (_.isObject(regexpDict) === false ) {
@@ -45,7 +36,6 @@ let g =getMatch(/j/,'t')
 console.log(g)
 
 module.exports = {
-  getData: getData,
   mapRegexps: mapRegexps,
   getMatch: getMatch
 } 
--- test/index.js
+++ test/index.js
@@ -10,31 +10,6 @@ var listing1 = fs.readFileSync('./test/fixtures/listing1.txt', "utf8");
 var listing2 = fs.readFileSync('./test/fixtures/listing2.txt', "utf8");
 let testRegexp = /(?:","truncated_localized_city":")([\w ]+)/
  
-describe('get data', function() {
-        
-    it('should return an object', function() {
-        expect(index.getData('teststring')).to.be.an('object');
-    });
-
-    it('should throw typerror when arg not of string type', function() {
-        expect(
-            function getData () {
-                index.getData(6);
-            }).to.throw(TypeError);
-
-        expect(
-            function getData () {
-                index.getData({'t':1});
-            }).to.throw(TypeError);
-
-        expect(
-            function getData() {
-                index.getData();
-            }).to.throw(TypeError);
-    });
-});
-
-
 describe('get match', function() {
     it('should be return an array', function() {
         expect(index.getMatch(testRegexp, listing1)).to.be.an('array');
