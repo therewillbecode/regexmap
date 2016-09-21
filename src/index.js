e9fb9d1ddcfc5f6d02f43bc3b2a66929eed6695a
Tom <tomw08@gmail.com>
2016-09-21 18:17:32 +0100

    Code style

--- src/index.js
+++ src/index.js
@@ -7,35 +7,35 @@ let listingRegexes = new listingRegexesClass;
 
 let fs = require('fs');
 
-
 let listing1 = fs.readFileSync('./test/fixtures/listing1.txt', "utf8");
-
 
 // maps dict like object of regular expressions to their match results for a given string
 let mapRegexps = (regexpDict, sourceTxt) => _.mapValues(regexpDict, (val, key, obj) => {
    if (_.isObject(regexpDict) === false ) {
-    throw TypeError('Arg1 must be object where property values are regexpb objs');
+    throw TypeError('Arg1 must be dict like object where property values are regular expression');
    }
 
    return getMatch(val, sourceTxt);
 });
 
+// statistics for which regexes worked
+
 // matches regex and returns given group
 let getMatch = (regexp, sourceTxt) => {
   let match = sourceTxt.match(regexp);
 
-
-  //if (_.isInteger(matchGroup) === true && _.isArray(match)){
- //   return _.nth(match, matchGroup)
- // } else{
     return match
- // }
 }
 
-let g =getMatch(/j/,'t')
-console.log(g)
+let testRegexpObjMultiProp = {
+        'city': /hj/, 
+        'lng': /(?:"lng":)([+-]?(?:\d*\.)?\d+)/,
+        'lat': /(?:"lat":)([+-]?(?:\d*\.)?\d+)/
+    };
+
 
 module.exports = {
   mapRegexps: mapRegexps,
   getMatch: getMatch
 } 
+
