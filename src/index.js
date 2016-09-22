195689b8caffd39fd3942b7858c20984328139b7
Tom <thomas.chambrier@astrosat.biz>
2016-09-22 08:58:55 +0100

    Added docstring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@
 
 const _ = require('lodash');
 
-// maps dict like object of regular expressions to their match results for a given string
+ /**
+   * Maps dict like object of regular expressions to their match results for a given string.
+   *
+   * @private
+   * @param {Object} Contains regex name and a regexp object i.e { name: /alexa/ } to iterate over.
+   * @param {String} The string for which to match the regexps in each property of object.
+   * @returns {Object} Returns the original object where property values are the result of the match.
+   */
 let mapRegexps = (regexpDict, sourceTxt) => _.mapValues(regexpDict, (val, key, obj) => {
    if (_.isObject(regexpDict) === false ) {
     throw TypeError('Arg1 must be dict like object where property values are regular expression');
